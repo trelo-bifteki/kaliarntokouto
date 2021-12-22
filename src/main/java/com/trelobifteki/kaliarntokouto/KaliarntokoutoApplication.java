a9bf41cc090a81d1369589faec6e7048d1ac135b
Lampros Papadimitriou <lambrospd@gmail.com>
2021-12-22 23:11:38 +0100

    initial commit

---
+++ src/main/java/com/trelobifteki/kaliarntokouto/KaliarntokoutoApplication.java
@@ -0,0 +1,13 @@
+package com.trelobifteki.kaliarntokouto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaliarntokoutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaliarntokoutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trelobifteki/kaliarntokouto/KaliarntokoutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trelobifteki.kaliarntokouto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaliarntokoutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
